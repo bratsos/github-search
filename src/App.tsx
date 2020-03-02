51b82f225199eeb6fd5d5675c5673f9d92b16172
Alex Bratsos <alexisbratsos@gmail.com>
2020-03-02 18:37:18 +0200

    Add app skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,121 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Wrapper, GlobalStyles } from './App.styles';
+import Autocomplete from './Autocomplete';
+
+const ACCESS_TOKEN = process.env.REACT_APP_GITHUB_KEY;
+
+interface GithubItem {
+  html_url: string,
+  login: string,
+  avatar_url: string
+};
+
+interface IState {
+  autocompleteValue: string,
+  autocompleteOptions: GithubItem[],
+  isLoading: boolean,
+}
+
+interface Action {
+  type: string,
+  payload?: GithubItem[] | string
+}
+
+const initialState = {
+  autocompleteValue: '',
+  autocompleteOptions: [],
+  isLoading: false,
+}
+
+const reducer = (state: IState = initialState, { type, payload }: Action): IState => {
+  switch (type) {
+    case 'reset':
+      return initialState;
+    case 'set_network_issues':
+      return {
+        ...state,
+        isLoading: false,
+      }
+    case 'start_loading':
+      return {
+        ...state,
+        isLoading: true
+      }
+    case 'set_results':
+      return {
+        ...state,
+        autocompleteOptions: Array.isArray(payload) ? payload : [],
+        isLoading: false,
+      }
+    case 'set_autocomplete_value':
+      return {
+        ...state,
+        autocompleteValue: typeof payload === 'string' ? payload : ''
+      }
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [{
+    autocompleteValue,
+    autocompleteOptions,
+    isLoading,
+  }, dispatch] = React.useReducer(reducer, initialState)
+
+  const resetState = () => dispatch({ type: 'reset' })
+
+  React.useEffect(() => {
+    let cancel: boolean = false;
+
+    if (!autocompleteValue) {
+      resetState();
+      return;
+    }
+
+    dispatch({ type: 'start_loading' })
+
+    const queryString = new URLSearchParams({
+      q: autocompleteValue,
+      access_token: ACCESS_TOKEN
+    }).toString();
+
+
+    fetch(`https://api.github.com/search/users?${queryString}`)
+        .then(response => response.json())
+        .then((response) => {
+          if (cancel) return;
+          
+          dispatch({
+            type: 'set_results',
+            payload: response.items
+          })
+
+          cancel = true;
+        })
+        .catch(() => resetState())
+
+    return () => { cancel = true }
+  }, [autocompleteValue])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <GlobalStyles />
+      <Wrapper>
+        <Autocomplete
+          onSearch={(text: string) => dispatch({ type: 'set_autocomplete_value', payload: text })}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {
+            autocompleteOptions.map((option) => {
+              return (
+                <Autocomplete.Item key={option.login}>{option.login}</Autocomplete.Item>
+              )
+            })
+          }
+        </Autocomplete>
+      </Wrapper>
+    </>
   );
 }
 
