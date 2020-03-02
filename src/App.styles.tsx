600b4a8f3ab0149fa012820f07c342054f61f825
Alex Bratsos <alexisbratsos@gmail.com>
2020-03-02 18:15:22 +0200

    Add styles

---
+++ src/App.styles.tsx
@@ -0,0 +1,37 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: -apple-system,
+      BlinkMacSystemFont,
+      'Segoe UI',
+      Roboto,
+      Helvetica,
+      Arial,
+      sans-serif,
+      'Apple Color Emoji',
+      'Segoe UI Emoji',
+      'Segoe UI Symbol';
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  #root {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(132deg, rgba(0,172,193,1) 0%, rgba(102,187,106,1) 100%);
+  }
+`;
+
+export const Wrapper = styled.main`
+  width: 90%;
+  max-width: 768px;
+  box-shadow: 0px 4px 4px rgba(0,0,0,.25);
+  min-height: 90vh;
+  background: #fff;
+`
